164e125e80d6e1c87b1ea846e3afb92d214e8133
Andreas Baierl <ichgeh@imkreisrum.de>
2022-09-28 13:15:59 +0200

    new VDR plugin: tvguide
    Signed-off-by: Andreas Baierl <ichgeh@imkreisrum.de>

---
+++ packages/vdr/_vdr-plugin-tvguide/package.mk
@@ -0,0 +1,46 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+PKG_NAME="_vdr-plugin-tvguide"
+PKG_VERSION="v1.3.6"
+PKG_SHA256="70bc12ff36e414e4c597cae6656ca06c855130e1dfa20be7c3f0b075b21967e1"
+PKG_LICENSE="GPL2"
+PKG_SITE="https://gitlab.com/kamel5/tvguide"
+PKG_URL="https://gitlab.com/kamel5/tvguide/-/archive/${PKG_VERSION}/tvguide-${PKG_VERSION}.tar.bz2"
+PKG_SOURCE_DIR="tvguide-${PKG_VERSION}"
+PKG_DEPENDS_TARGET="toolchain _vdr _graphicsmagick"
+PKG_NEED_UNPACK="$(get_pkg_directory _vdr)"
+PKG_LONGDESC="TODO"
+PKG_TOOLCHAIN="manual"
+
+pre_configure_target() {
+  export LDFLAGS="$(echo ${LDFLAGS} | sed -e "s|-Wl,--as-needed||") -L${SYSROOT_PREFIX}/usr/local/lib"
+}
+
+make_target() {
+  VDR_DIR=$(get_build_dir _vdr)
+  export PKG_CONFIG_PATH=${VDR_DIR}:${SYSROOT_PREFIX}/usr/local/lib/pkgconfig:${PKG_CONFIG_PATH}
+  export CPLUS_INCLUDE_PATH=${VDR_DIR}/include
+
+  make
+}
+
+makeinstall_target() {
+  LIB_DIR=${INSTALL}/$(pkg-config --variable=locdir vdr)/../../lib/vdr
+  make DESTDIR="${INSTALL}" LIBDIR="${LIB_DIR}" install
+}
+
+post_makeinstall_target() {
+  mkdir -p ${INSTALL}/storage/.config/vdropt-sample/conf.d
+  cp -PR ${PKG_DIR}/conf.d/* ${INSTALL}/storage/.config/vdropt-sample/conf.d/
+
+  if find ${INSTALL}/storage/.config/vdropt -mindepth 1 -maxdepth 1 2>/dev/null | read; then
+    cp -ar ${INSTALL}/storage/.config/vdropt/* ${INSTALL}/storage/.config/vdropt-sample
+    rm -Rf ${INSTALL}/storage/.config/vdropt
+  fi
+
+  # create config.zip
+  VERSION=$(pkg-config --variable=apiversion vdr)
+  cd ${INSTALL}
+  mkdir -p ${INSTALL}/usr/local/config/
+  zip -qrum9 "${INSTALL}/usr/local/config/tvguide-sample-config.zip" storage
+}

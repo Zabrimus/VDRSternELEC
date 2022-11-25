c17f8203912da7ddfa38cca2e7abfb0a207ef838
Reini-at <76917598+reinhardsiegel79@users.noreply.github.com>
2022-11-25 11:22:24 +0100

    Update switch_vdr_softhdodroid.sh
    add cecremote control - disable vdr cecremote when switching to kodi and vice versa

--- packages/vdr/_vdr/bin/switch_vdr_softhdodroid.sh
+++ packages/vdr/_vdr/bin/switch_vdr_softhdodroid.sh
@@ -9,9 +9,11 @@
 if [ "$1" = "attach" ]; then
     echo 4 > /sys/module/amvdec_h264/parameters/dec_control
     /usr/local/bin/svdrpsend REMO on
+    /usr/local/bin/svdrpsend PLUG cecremote CONN
     /usr/local/bin/svdrpsend PLUG softhdodroid ATTA -a hw:CARD=AMLAUGESOUND,DEV=2
 elif [ "$1" = "detach" ]; then
     /usr/local/bin/svdrpsend PLUG softhdodroid DETA
     /usr/local/bin/svdrpsend REMO off
+    /usr/local/bin/svdrpsend PLUG cecremote DISC
     echo rm pip0 > /sys/class/vfm/map
 fi

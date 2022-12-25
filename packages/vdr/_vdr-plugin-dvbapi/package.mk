8ccd4f04db1738a96dfe114a5a03fdb58780170f
Zabrimus <github@hannebauer.org>
2022-12-25 12:42:30 +0100

    plugin update: dvbapi

--- packages/vdr/_vdr-plugin-dvbapi/package.mk
+++ packages/vdr/_vdr-plugin-dvbapi/package.mk
@@ -1,12 +1,12 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 PKG_NAME="_vdr-plugin-dvbapi"
-PKG_VERSION="c5f3648c8f0bb7213280253bad106a6a3e113b9e"
-PKG_SHA256="4a8eb5b645f0dcd4d6333ed39c0cc7ae5686c43c2e369e53691e51f3a857ed3a"
+PKG_VERSION="28fc0c9670427d74010365772077b507bdd5e096"
+PKG_SHA256="183356e9e877345f54e6df0e9a274b367e649438003619845ebff485cb5dbf3c"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/manio/vdr-plugin-dvbapi"
 PKG_URL="https://github.com/manio/vdr-plugin-dvbapi/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain _vdr libdvbcsa"
+PKG_DEPENDS_TARGET="toolchain _vdr _libdvbcsa"
 PKG_NEED_UNPACK="$(get_pkg_directory _vdr)"
 PKG_LONGDESC="VDR dvbapi plugin for use with OSCam"
 PKG_TOOLCHAIN="manual"
@@ -20,7 +20,7 @@ make_target() {
   export PKG_CONFIG_PATH=${VDR_DIR}:${SYSROOT_PREFIX}/usr/local/lib/pkgconfig:${PKG_CONFIG_PATH}
   export CPLUS_INCLUDE_PATH=${VDR_DIR}/include
 
-  make LIBDVBCSA=1
+  make LIBDVBCSA_NEW=1
 }
 
 makeinstall_target() {
---
+++ packages/vdr/vdr-depends/_libdvbcsa/package.mk
@@ -0,0 +1,30 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="_libdvbcsa"
+PKG_VERSION="8d3ab76a601bf302188fdfe5da5a1849a3eaefbb"
+PKG_SHA256="8b9e1ba9ff9cae8e085e52872f4a9dcadea4d80273a1026ebd60c27658a77d36"
+PKG_LICENSE="LGPL"
+PKG_SITE="https://www.videolan.org/developers/libdvbcsa.html"
+PKG_URL="https://github.com/pinkflozd/libdvbcsa/archive/${PKG_VERSION}.zip"
+PKG_SOURCE_DIR="libdvbcsa-${PKG_VERSION}"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="A implementation of DVB/CSA, with encryption and decryption capabilities."
+PKG_TOOLCHAIN="autotools"
+# libdvbcsa is a bit faster without LTO, and tests will fail with gcc-5.x
+PKG_BUILD_FLAGS="+pic"
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static --with-sysroot=${SYSROOT_PREFIX}"
+
+if target_has_feature neon; then
+  PKG_CONFIGURE_OPTS_TARGET+=" --enable-neon"
+elif [ "${TARGET_ARCH}" = x86_64  ]; then
+  if target_has_feature ssse3; then
+    PKG_CONFIGURE_OPTS_TARGET+=" --enable-ssse3"
+  elif target_has_feature sse2; then
+    PKG_CONFIGURE_OPTS_TARGET+=" --enable-sse2"
+  else
+    PKG_CONFIGURE_OPTS_TARGET+=" --enable-uint64"
+  fi
+fi

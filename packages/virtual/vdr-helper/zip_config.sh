a29a314f9bc331266418c52cde61137bdcf514d2
Andreas Baierl <ichgeh@imkreisrum.de>
2023-05-10 11:29:13 +0200

    vdr-helper: create default plugin confs on the fly if they don't exist
    Signed-off-by: Andreas Baierl <ichgeh@imkreisrum.de>

--- packages/virtual/vdr-helper/zip_config.sh
+++ packages/virtual/vdr-helper/zip_config.sh
@@ -4,8 +4,30 @@ INSTALL=$1
 PKG_DIR=$2
 PLUGIN=$3
 
-mkdir -p ${INSTALL}/storage/.config/vdropt-sample/conf.d
-cp -PR ${PKG_DIR}/conf.d/* ${INSTALL}/storage/.config/vdropt-sample/conf.d/
+# create default ${PLUGIN}.conf // will be overwritten, if we have something in the plugin directory
+mkdir -p ${INSTALL}/storage/.config/vdropt-sample/conf.d/
+cat > ${INSTALL}/storage/.config/vdropt-sample/conf.d/${PLUGIN}.conf <<EOF
+[${PLUGIN}]
+
+EOF
+
+# create default ${PLUGIN}_settings.ini // will be overwritten, if we have something in the plugin directory
+cat > ${INSTALL}/storage/.config/vdropt-sample/conf.d/${PLUGIN}_settings.ini <<EOF
+[EasyPluginManager]
+AutoRun = false
+Stop = true
+Args =
+
+EOF
+
+# hack: streamdev names its confs streamdev-server/client.conf, so remove the defaults again
+if [ ${PLUGIN} = "streamdev" ]; then
+  rm ${INSTALL}/storage/.config/vdropt-sample/conf.d/${PLUGIN}.conf
+  rm ${INSTALL}/storage/.config/vdropt-sample/conf.d/${PLUGIN}_settings.ini
+fi
+
+# copy plugin conf.d/* to sample config dir
+cp -PR ${PKG_DIR}/conf.d/* ${INSTALL}/storage/.config/vdropt-sample/conf.d/ 2>/dev/null
 
 if find ${INSTALL}/storage/.config/vdropt -mindepth 1 -maxdepth 1 2>/dev/null | read; then
   cp -ar ${INSTALL}/storage/.config/vdropt/* ${INSTALL}/storage/.config/vdropt-sample

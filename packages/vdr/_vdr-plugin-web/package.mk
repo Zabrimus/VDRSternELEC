61568f556794941068afd58e169e9d0021dff5f9
Zabrimus <github@hannebauer.org>
2023-07-27 17:46:16 +0200

    plugin updates: web

--- packages/vdr/_vdr-plugin-web/package.mk
+++ packages/vdr/_vdr-plugin-web/package.mk
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0
 
 PKG_NAME="_vdr-plugin-web"
-PKG_VERSION="eb2bc33822aa2e8e2ee38a2239f5399f60e6ecca"
-PKG_SHA256="2d4215ea00e85c9c282822d0a3f0fc420c3ed765d028fa56935141ebd1142290"
+PKG_VERSION="c4ce65a1673353b8a8549b46ef3e56cb44a41c38"
+PKG_SHA256="c2f06e778936bf70927f41ec33d862bc88275cf736d14166b0fde5235830fcef"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/Zabrimus/vdr-plugin-web"
 PKG_URL="https://github.com/Zabrimus/vdr-plugin-web/archive/${PKG_VERSION}.zip"
@@ -12,6 +12,7 @@ PKG_DEPENDS_CONFIG="_vdr"
 PKG_SOURCE_DIR="vdr-plugin-web-${PKG_VERSION}"
 PKG_NEED_UNPACK="$(get_pkg_directory _vdr vdr-helper)"
 PKG_LONGDESC="HbbTV Plugin. Needs cefbrowser and remotetranscoder"
+PKG_MAKE_OPTS_TARGET="ENABLE_FAST_SCALE=1"
 
 pre_make_target() {
   export LDFLAGS="$(echo ${LDFLAGS} | sed -e "s|-Wl,--as-needed||") -L${SYSROOT_PREFIX}/usr/local/lib"

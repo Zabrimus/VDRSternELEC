2b6fba6dadc4241cf8ce8ebd8f9765f132e79c9a
Andreas Baierl <ichgeh@imkreisrum.de>
2023-08-29 09:55:27 +0200

    update plugins:
    softhddevice-drm-gles
    
    Signed-off-by: Andreas Baierl <ichgeh@imkreisrum.de>

--- packages/vdr/_vdr-plugin-softhddevice-drm-gles/package.mk
+++ packages/vdr/_vdr-plugin-softhddevice-drm-gles/package.mk
@@ -2,13 +2,13 @@
 
 PKG_NAME="_vdr-plugin-softhddevice-drm-gles"
 
-PKG_VERSION="e6abd0e7e715b63c8823e78d647c48a5c99dcc7b"
-PKG_SHA256="ab1176532b9a304a69ef4c4be2f3404ca44c83beeef0ea92c3a4683de6ae740d"
-PKG_SITE="https://github.com/rellla/vdr-plugin-softhddevice-drm"
-PKG_URL="https://github.com/rellla/vdr-plugin-softhddevice-drm/archive/${PKG_VERSION}.zip"
+PKG_VERSION="f84861f9715cc7a790293e3ac658070425dd0972"
+PKG_SHA256="7928162832a99abb845f8f0093a2a9dc9b98491490f117b8c47bdf16c0e68f63"
+PKG_SITE="https://github.com/rellla/vdr-plugin-softhddevice-drm-gles"
+PKG_URL="https://github.com/rellla/vdr-plugin-softhddevice-drm-gles/archive/${PKG_VERSION}.zip"
 PKG_BRANCH="drm-atomic-gles"
 PKG_LICENSE="GPL"
-PKG_SOURCE_DIR="vdr-plugin-softhddevice-drm-${PKG_VERSION}"
+PKG_SOURCE_DIR="vdr-plugin-softhddevice-drm-gles-${PKG_VERSION}"
 PKG_DEPENDS_TARGET="toolchain glm alsa freetype ffmpeg _vdr libdrm mesa vdr-helper"
 PKG_DEPENDS_CONFIG="_vdr"
 PKG_NEED_UNPACK="$(get_pkg_directory _vdr vdr-helper)"

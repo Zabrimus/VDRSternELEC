05f59bb1143e2860a3f111c30639b673e59cdfbf
Zabrimus <github@hannebauer.org>
2023-10-02 16:58:13 +0200

    fix build (thx to rell at vdr-portal.de)

--- packages/tools/_python-evdev/package.mk
+++ packages/tools/_python-evdev/package.mk
@@ -1,33 +1,32 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="_python-evdev"
 PKG_VERSION="2dd6ce6364bb67eedb209f6aa0bace0c18a3a40a"
 PKG_SHA256="b802f9e25c6c218facd54780c4b3d6436fb769d51e3aa286df63a382a4c799ab"
-PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/gvalkov/python-evdev"
+PKG_LICENSE=""
+PKG_SITE="https://github.com/gvalkov/python-evdev/"
 PKG_URL="https://github.com/gvalkov/python-evdev/archive/${PKG_VERSION}.zip"
-PKG_BRANCH="master"
-PKG_SOURCE_DIR="python-evdev-${PKG_VERSION}"
 PKG_DEPENDS_TARGET="toolchain libevdev Python3 distutilscross:host"
+PKG_SOURCE_DIR="python-evdev-${PKG_VERSION}"
 PKG_LONGDESC="Python bindings for the Linux input subsystem"
 PKG_TOOLCHAIN="manual"
 
 pre_configure_target() {
-  export PYTHONXCPREFIX="${SYSROOT_PREFIX}/usr"
   export LDSHARED="${CC} -shared"
-  export CFLAGS="${CFLAGS} -fcommon"
-  export CPPFLAGS="-I${SYSROOT_PREFIX}/usr/include/${PKG_PYTHON_VERSION} -I$(get_install_dir glibc)/usr/include -I${SYSROOT_PREFIX}/usr/include/linux  ${TARGET_CPPFLAGS} "
 }
 
 make_target() {
-  python3 setup.py build_ext
+  python setup.py \
+         build \
+         build_ecodes --evdev-headers ${SYSROOT_PREFIX}/usr/include/linux/input.h:${SYSROOT_PREFIX}/usr/include/linux/input-event-codes.h \
+         build_ext --include-dirs ${SYSROOT_PREFIX}/
 }
 
 makeinstall_target() {
-  PYTHONXCPREFIX="${SYSROOT_PREFIX}/usr" python3 setup.py install --root=$INSTALL --prefix=/usr
+  python setup.py install --root=$INSTALL/usr --home=""
 }
 
-#post_makeinstall_target() {
-#  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
-#  rm -rf $INSTALL/usr/lib/python*/site-packages/*/tests
-#}
+post_makeinstall_target() {
+  find ${INSTALL}/usr/lib -name "*py" -exec rm -rf "{}" ";"
+}

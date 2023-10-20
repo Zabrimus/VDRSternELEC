519041f496dd17d607fdbcb78c680b7237e4917e
Zabrimus <github@hannebauer.org>
2023-10-20 11:35:10 +0200

    build amremote only for CoreELEC

--- packages/virtual/tools/package.mk
+++ packages/virtual/tools/package.mk
@@ -9,4 +9,8 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="virtual"
 PKG_LONGDESC="Tools which can be useful"
 
-PKG_DEPENDS_TARGET+=" _triggerhappy _python-evdev _amremote"
+PKG_DEPENDS_TARGET+=" _triggerhappy _python-evdev"
+
+if [ "${DISTRO}" = "CoreELEC" ]; then
+	PKG_DEPENDS_TARGET+=" _amremote"
+fi

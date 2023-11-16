96a91e3c8bf40e372d5e2e3c950990d7b25a8254
Andreas Baierl <ichgeh@imkreisrum.de>
2023-11-16 17:25:57 +0100

    update.sh: some improvements

--- update.sh
+++ update.sh
@@ -77,20 +77,40 @@ update() {
   if [[ -z ${RUN} ]]; then
      echo "Perform a dry run!"
   else
-     echo "Try updating packages!"
+     echo "Try update ..."
   fi
 
   if [[ -z "$PACKAGE" ]]; then
-    echo "Count: ${#packages[@]}"
+    echo "Available packages: ${#packages[@]}"
   else
     echo "checking $PACKAGE"
   fi
 
+  PKGARRAY=()
+  VERARRAY=()
+  SHAARRAY=()
+  PKGNAMEARRAYMAN=()
+  PKGNAMEARRAYSKIP=()
+  manual=0
+  skipped=0
+  autoupdate=0
+
   for (( i=0; i<${#packages[@]}; i++ )); do
     PKG_BRANCH=""
     PKG_BRANCH_DEFAULT=""
-    source ${packages[$i]}
+
+    PKG_NAME=$(grep -e "^PKG_NAME" ${packages[$i]} | sed s/^PKG_NAME=\"// | sed s/\"$// )
+    PKG_SITE=$(grep -e "^PKG_SITE" ${packages[$i]} | sed s/^PKG_SITE=\"// | sed s/\"$// )
+    PKG_BRANCH=$(grep -e "^PKG_BRANCH" ${packages[$i]} | sed s/^PKG_BRANCH=\"// | sed s/\"$// )
+    PKG_VERSION=$(grep -e "^PKG_VERSION" ${packages[$i]} | sed s/^PKG_VERSION=\"// | sed s/\"$// )
+    PKG_URL=$(grep -e "^PKG_URL" ${packages[$i]} | sed s/^PKG_URL=\"// | sed s/\"$// )
+    PKG_BRANCH=$(grep -e "^PKG_BRANCH" ${packages[$i]} | sed s/^PKG_BRANCH=\"// | sed s/\"$// )
+
     if [ -n "$PACKAGE" ] && [ ! "$PACKAGE" = "$PKG_NAME" ]; then
+        continue
+    fi
+
+    if [[ ! -z "$PLUGINSONLY" ]] && [[ ! $PKG_NAME =~ "_vdr-plugin" ]]; then
         continue
     fi
 
@@ -103,29 +123,65 @@ update() {
       fi
 
       LATEST=$(git ls-remote -h $PKG_SITE | grep $PKG_BRANCH | head -1 | awk -F' '  '{ print $1 }')
-
       if [ ! "$PKG_VERSION" == "$LATEST" ]; then
-          echo "$PKG_NAME - found new version"
-          echo "      PKG_VERSION=\"$LATEST\""
-
-          NEUE_URL=$( echo $PKG_URL | sed -e s/${PKG_VERSION}/${LATEST}/g )
-          SHA=$(wget -q "${NEUE_URL}" -O- | sha256sum | cut -d " " -f1)
-          echo "      PKG_SHA256=\"${SHA}\""
-
           if [[ -z `grep -oE "^PKG_VERSION=\"([a-f]|[0-9]){40}\"" ${packages[$i]}` ]]; then
-              echo "      --> packages version is a release tag, skip updating"
+              echo "   $PKG_NAME - found new version, but packages version is a release tag, skip updating"
+              PKGNAMEARRAYMAN+=( ${PKG_NAME} )
+              manual=$((manual + 1))
               continue
           fi
 
-          if [[ -n $RUN ]]; then
-              echo "      --> replacing PKG_VERSION and PKG_SHA256 in ${packages[$i]}"
-              sed -i "s/^PKG_VERSION=.*/PKG_VERSION=\"${LATEST}\"/" ${packages[$i]}
-              sed -i "s/^PKG_SHA256=.*/PKG_SHA256=\"${SHA}\"/" ${packages[$i]}
-          fi
+          echo "$PKG_NAME - found new version"
+          NEUE_URL=$( echo ${PKG_URL} | sed -e s/\$\{PKG_VERSION\}/${LATEST}/g )
+          SHA=$(wget -q "${NEUE_URL}" -O- | sha256sum | cut -d " " -f1)
+          echo "      PKG_VERSION=\"$LATEST\""
+          echo "      PKG_SHA256=\"${SHA}\""
+
+          # fill array with updateable packages
+          PKGARRAY+=( ${packages[$i]} )
+          PKGNAMEARRAY+=( ${PKG_NAME} )
+          VERARRAY+=( ${LATEST} )
+          SHAARRAY+=( ${SHA} )
+          autoupdate=$((autoupdate + 1))
       else
-          echo "$PKG_NAME - no update available"
+          echo "   ## $PKG_NAME - no update available"
+          noupdate=$((noupdate + 1))
       fi
+    else
+      PKGNAMEARRAYSKIP+=( ${PKG_NAME} )
+      skipped=$((skipped + 1))
     fi
+  done
+
+  # update packages
+  if [ -z "$PACKAGE" ]; then
+     echo "Auto update possible ($autoupdate):"
+  fi
+  if [[ -n $RUN ]]; then
+     for j in ${!PKGARRAY[@]}; do
+         echo "--> replacing PKG_VERSION and PKG_SHA256 for ${PKGNAMEARRAY[$j]}"
+         sed -i "s/^PKG_VERSION=.*/PKG_VERSION=\"${VERARRAY[$j]}\"/" ${PKGARRAY[$j]}
+         sed -i "s/^PKG_SHA256=.*/PKG_SHA256=\"${SHAARRAY[$j]}\"/" ${PKGARRAY[$j]}
+     done
+  else
+     for j in ${!PKGARRAY[@]}; do
+         echo "--> update available for ${PKGNAMEARRAY[$j]} -> ${VERARRAY[$j]} (${SHAARRAY[$j]})"
+     done
+  fi
+
+  if [ -n "$PACKAGE" ]; then
+     exit 0
+  fi
+
+  echo "Manual update possible ($manual):"
+  for j in ${!PKGNAMEARRAYMAN[@]}; do
+      echo "--> manual update available for ${PKGNAMEARRAYMAN[$j]}"
+  done
+
+  echo "No update available ($noupdate):"
+  echo "Skipped packages ($skipped):"
+  for j in ${!PKGNAMEARRAYSKIP[@]}; do
+      echo "--> skipped check for ${PKGNAMEARRAYSKIP[$j]}"
   done
 }
 
@@ -135,6 +191,12 @@ try_update() {
 
 update_all() {
   RUN="1"
+  update
+}
+
+update_plugins_only() {
+  RUN="1"
+  PLUGINSONLY="1"
   update
 }
 
@@ -158,6 +220,7 @@ fi
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -a) update_all ;;
+        -p) update_plugins_only ;;
         -t) try_update ;;
         -s) shift; update_package $1 ;;
         -r) shift; reset_package $1 ;;

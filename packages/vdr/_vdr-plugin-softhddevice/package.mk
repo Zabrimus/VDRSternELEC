abd8fd05e2806d06c9e0e6685a141c3bd6dca7ab
Zabrimus <github@hannebauer.org>
2023-12-08 17:24:09 +0100

    update: softhddevice

--- packages/vdr/_vdr-plugin-softhddevice/package.mk
+++ packages/vdr/_vdr-plugin-softhddevice/package.mk
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 PKG_NAME="_vdr-plugin-softhddevice"
-PKG_VERSION="35a27bc32ac43f61ec49f8e21f6e1f92b0aac077"
-PKG_SHA256="0f8a67ded4e41120561218f1bca8f33ce9456ac62edef241b4f26d3f67ceb28a"
+PKG_VERSION="28bd06d3bbd4af0a04ef9570c527976043db1f50"
+PKG_SHA256="3b342cedaaa43e4e4b675daf0875af081f25a47318b927bbd1fc5b6661021ee8"
 PKG_LICENSE="AGPLv3"
 PKG_SITE="https://github.com/ua0lnj/vdr-plugin-softhddevice"
 PKG_URL="https://github.com/ua0lnj/vdr-plugin-softhddevice/archive/${PKG_VERSION}.zip"

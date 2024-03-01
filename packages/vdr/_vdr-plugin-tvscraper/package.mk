3e52a90d45b63018432a8653995a3dfa03ec601f
Zabrimus <github@hannebauer.org>
2024-03-01 21:28:59 +0100

    update: tvscraper

--- packages/vdr/_vdr-plugin-tvscraper/package.mk
+++ packages/vdr/_vdr-plugin-tvscraper/package.mk
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 PKG_NAME="_vdr-plugin-tvscraper"
-PKG_VERSION="d963d46ca00f64de88d9bbf901f6d5fc8256ef17"
-PKG_SHA256="7674fd8cd4f4c380f1ca850fc3e8ae839d089733827bb98a169a3dc1021f7316"
+PKG_VERSION="585177809cb44b9f99caa55a7f83657a17865be7"
+PKG_SHA256="b98820216b44e9dbd44a61b4f498a2ba15eca51a49f331a2bd593b4054a42491"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/MarkusEh/vdr-plugin-tvscraper"
 PKG_URL="https://github.com/MarkusEh/vdr-plugin-tvscraper/archive/${PKG_VERSION}.zip"

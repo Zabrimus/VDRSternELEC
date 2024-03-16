bde9003d58f506f12211f85b40c517126f9be02a
Zabrimus <github@hannebauer.org>
2024-03-16 12:00:21 +0100

    change PKG_URL of _a52dec

--- packages/vdr/vdr-depends/_a52dec/package.mk
+++ packages/vdr/vdr-depends/_a52dec/package.mk
@@ -1,14 +1,14 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 PKG_NAME="_a52dec"
-PKG_VERSION="0.7.4"
-PKG_SHA256="a21d724ab3b3933330194353687df82c475b5dfb997513eef4c25de6c865ec33"
+PKG_VERSION="0.8.0"
+PKG_SHA256="470d084074d15416a0f0f300b85b61ddcc1ce105ad14a2aac670a1767ecba8de"
 PKG_LICENSE="GPL"
-PKG_SITE="https://liba52.sourceforge.io"
-PKG_URL="https://liba52.sourceforge.io/files/a52dec-${PKG_VERSION}.tar.gz"
+PKG_SITE="https://git.adelielinux.org/community/a52dec"
+PKG_URL="https://git.adelielinux.org/community/a52dec/-/archive/v0.8.0/a52dec-v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain _libdvdread"
 PKG_LONGDESC="liba52 - a free ATSC A/52 stream decoder"
-PKG_TOOLCHAIN="auto"
+PKG_TOOLCHAIN="configure"
 PKG_BUILD_FLAGS="+speed"
 
 PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr/local \
@@ -27,6 +27,7 @@ PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr/local \
 
 pre_configure_target() {
   export LDFLAGS="$(echo ${LDFLAGS} | sed -e "s|-Wl,--as-needed||") -L${SYSROOT_PREFIX}/usr/local/lib"
+  ./bootstrap
 }
 
 make_target() {

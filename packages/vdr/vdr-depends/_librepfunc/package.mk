6bc5a4ea266a61dcd70a9f983e322e9e5c75faca
Zabrimus <github@hannebauer.org>
2024-05-02 04:56:49 +0200

    update: librepfunc

--- packages/vdr/vdr-depends/_librepfunc/package.mk
+++ packages/vdr/vdr-depends/_librepfunc/package.mk
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 PKG_NAME="_librepfunc"
-PKG_VERSION="1.9.0"
-PKG_SHA256="2eb8a4d9c54e8383d96865448a5090875c0168830964f39014b0287a4e2657d0"
+PKG_VERSION="1.10.0"
+PKG_SHA256="428645c7c9c5294b366c01cb79cc9c2f57ec6b030a5744dd68f3953519e24c23"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/wirbel-at-vdr-portal/librepfunc"
 PKG_URL="https://github.com/wirbel-at-vdr-portal/librepfunc/archive/${PKG_VERSION}.zip"

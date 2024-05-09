b325d9354b25c456e00490c0961d6826b2343820
Zabrimus <github@hannebauer.org>
2024-05-09 16:09:27 +0200

    cleanup and reorganize CE patches

---
+++ patches/CoreELEC/coreelec-22/0002-media_modules-aml.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+# delete patch
+rm ./projects/Amlogic-ce/devices/Amlogic-ne/packages/linux-drivers/amlogic/media_modules-aml/patches/003-multi-decoders-limit-maximum-number-of-decoder.patch
+rm ./projects/Amlogic-ce/devices/Amlogic-no/packages/linux-drivers/amlogic/media_modules-aml/patches/003-multi-decoders-limit-maximum-number-of-decoder.patch

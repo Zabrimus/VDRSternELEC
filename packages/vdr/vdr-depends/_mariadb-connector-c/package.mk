dbe7ed35ae853f1aa94a16cda5036eadb93493f2
Zabrimus <github@hannebauer.org>
2024-05-23 14:50:58 +0200

    fix several build problems (libcddb needs more investigation, vdr-plugin-cdplayer disabled)

--- packages/vdr/vdr-depends/_mariadb-connector-c/package.mk
+++ packages/vdr/vdr-depends/_mariadb-connector-c/package.mk
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0
 
 PKG_NAME="_mariadb-connector-c"
-PKG_VERSION="3.2.6"
-PKG_SHA256="d9aee070e1d954074a70eff1cfd2d6c6bc5d190dc00075975a55d4f3e1ac20c5"
+PKG_VERSION="3.3.10"
+PKG_SHA256="0a79088af2fbde4dbe6655dbc51bbb272b606c0d9116745697e08879e70198a7"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://mariadb.org/"
 PKG_URL="https://github.com/mariadb-corporation/mariadb-connector-c/archive/refs/tags/v${PKG_VERSION}.tar.gz"

0a3b2057ec1119dd9f2f211b6ccb889632000389
Zabrimus <github@hannebauer.org>
2024-06-08 21:50:49 +0200

    compile driver PCF8563 as module

--- patches/CoreELEC/coreelec-22/0004-meson-vrtc-driver.sh
+++ patches/CoreELEC/coreelec-22/0004-meson-vrtc-driver.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-sed -i -e "s/# CONFIG_RTC_DRV_PCF8563 is not set/CONFIG_RTC_DRV_PCF8563=y/" ./projects/Amlogic-ce/devices/Amlogic-no/linux/linux.aarch64.conf
+sed -i -e "s/# CONFIG_RTC_DRV_PCF8563 is not set/CONFIG_RTC_DRV_PCF8563=m/" ./projects/Amlogic-ce/devices/Amlogic-no/linux/linux.aarch64.conf
 
 echo "CONFIG_RTC_DRV_MESON_VRTC=m"  >> ./projects/Amlogic-ce/devices/Amlogic-ne/linux/linux.aarch64.conf

7d3e8e115ceeab0f65bd25db487a7e601493b565
Zabrimus <github@hannebauer.org>
2024-06-12 18:51:34 +0200

    addon incus: cleanup

--- packages/virtual/tools/package.mk
+++ packages/virtual/tools/package.mk
@@ -14,7 +14,3 @@ PKG_DEPENDS_TARGET+=" _triggerhappy _python-evdev _irmplircd _vtuner-ng"
 if [ "${DISTRO}" = "CoreELEC" ]; then
 	PKG_DEPENDS_TARGET+=" _amremote"
 fi
-
-# Addon Depends which shall be installed in the base image
-# It's much easier to build, package and maintain
-# PKG_DEPENDS_TARGET+=" _cowsql _dnsmasq _libacl _lxc _lxcfs _raft _squashfs-tools _tar _zstd"

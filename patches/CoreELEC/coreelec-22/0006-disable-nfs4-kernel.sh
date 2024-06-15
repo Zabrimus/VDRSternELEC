13c2c0de585430e129184424d97c79a1a2345383
Zabrimus <github@hannebauer.org>
2024-06-15 16:32:34 +0200

    delete patch which disabled NFS v4

--- patches/CoreELEC/coreelec-22/0006-disable-nfs4-kernel.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-sed -i -e "s/CONFIG_NFS_V4=y/# CONFIG_NFS_V4 is not set/" ./projects/Amlogic-ce/devices/Amlogic-no/linux/linux.aarch64.conf
-sed -i -e "s/CONFIG_NFSD_V4=y/# CONFIG_NFSD_V4 is not set/" ./projects/Amlogic-ce/devices/Amlogic-no/linux/linux.aarch64.conf

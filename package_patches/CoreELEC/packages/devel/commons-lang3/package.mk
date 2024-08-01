3af05f4d29cc6663aa5ff480a9f71cfddbc95ea4
Zabrimus <github@hannebauer.org>
2024-08-01 12:12:23 +0200

    update CE22, LE12, LE13

---
+++ package_patches/CoreELEC/packages/devel/commons-lang3/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="commons-lang3"
+PKG_VERSION="3.15.0"
+PKG_SHA256="5da23a6a328803936263d52e64974c0ae6769084f771b8ada4c5c98f7de5c84e"
+PKG_LICENSE="Apache-2.0"
+PKG_SITE="https://commons.apache.org/proper/commons-lang/"
+PKG_URL="https://dlcdn.apache.org/commons/lang/binaries/commons-lang3-${PKG_VERSION}-bin.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Apache Commons Lang provides a host of helper utilities for the java.lang API"
+PKG_TOOLCHAIN="manual"

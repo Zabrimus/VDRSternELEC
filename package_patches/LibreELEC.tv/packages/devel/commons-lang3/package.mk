9241788b95f40cbe21d8f552440e570fcf0fef7a
Zabrimus <github@hannebauer.org>
2024-09-01 12:11:05 +0200

    fix commons-lang3 URL

--- package_patches/LibreELEC.tv/packages/devel/commons-lang3/package.mk
+++ package_patches/LibreELEC.tv/packages/devel/commons-lang3/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="commons-lang3"
-PKG_VERSION="3.16.0"
-PKG_SHA256="0c97775a9e8a37b1e4cfd0993ad27c28bf6fc2611857a80ff5769805cea53a0a"
+PKG_VERSION="3.17.0"
+PKG_SHA256="924d95a2792478457e59ef062e3d4b508febf60aef779b7c0a6a0e057c9e33a9"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://commons.apache.org/proper/commons-lang/"
-PKG_URL="https://dlcdn.apache.org/commons/lang/binaries/commons-lang3-${PKG_VERSION}-bin.tar.gz"
+PKG_URL="https://archive.apache.org/dist/commons/lang/binaries/commons-lang3-${PKG_VERSION}-bin.zip"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Apache Commons Lang provides a host of helper utilities for the java.lang API"
 PKG_TOOLCHAIN="manual"

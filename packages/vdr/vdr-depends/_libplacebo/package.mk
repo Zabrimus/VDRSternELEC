b4bf576ad37ba0380cadafcee322d5b960771cd3
Andreas Baierl <ichgeh@imkreisrum.de>
2024-09-11 13:41:01 +0200

    use upstream glslang and bump libshaderc
    fix

--- packages/vdr/vdr-depends/_libplacebo/package.mk
+++ packages/vdr/vdr-depends/_libplacebo/package.mk
@@ -8,7 +8,7 @@ PKG_SHA256="5090fbad4f65b9c950028c1e0dd86d20aa66d159c0854a89b3eb8d4210966b73"
 PKG_LICENSE="LGPL2.1"
 PKG_SITE="https://code.videolan.org/videolan/libplacebo"
 PKG_URL="https://github.com/haasn/libplacebo/archive/refs/tags/v${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain spirv-headers spirv-tools lcms2 _libshaderc libepoxy"
+PKG_DEPENDS_TARGET="toolchain glslang lcms2 _libshaderc libepoxy"
 PKG_LONGDESC="eusable library for GPU-accelerated image/video processing primitives and shaders,"
 PKG_BUILD_FLAGS="+pic +speed"
 

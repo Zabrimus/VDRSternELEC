cd4ea92d8a03845f4925f8e580b8d98339a31e65
Andreas Baierl <ichgeh@imkreisrum.de>
2024-09-16 12:40:47 +0200

    fix vdr-plugin-softhddrm,cuvid,vaapi build

--- packages/vdr/_vdr-plugin-softhddrm/package.mk
+++ packages/vdr/_vdr-plugin-softhddrm/package.mk
@@ -8,7 +8,7 @@ PKG_SITE="https://github.com/jojo61/vdr-plugin-softhdcuvid"
 PKG_URL="https://github.com/jojo61/vdr-plugin-softhdcuvid/archive/${PKG_VERSION}.zip"
 PKG_SOURCE_DIR="vdr-plugin-softhdcuvid-${PKG_VERSION}"
 PKG_DEPENDS_TARGET="toolchain _vdr mesa glu glew _freeglut libxcb libX11 _xcb-util-wm _libplacebo vdr-helper"
-PKG_DEPENDS_CONFIG="_vdr"
+PKG_DEPENDS_CONFIG="_vdr _libplacebo"
 PKG_NEED_UNPACK="$(get_pkg_directory _vdr vdr-helper)"
 PKG_LONGDESC="VDR Output Device (softhdddrm"
 PKG_BUILD_FLAGS="+speed"
@@ -28,6 +28,5 @@ pre_make_target() {
 post_makeinstall_target() {
   mkdir -p ${INSTALL}/storage/.config/vdropt-sample/plugins/softhddrm
 
-  PLUGIN="$(cat ${PKG_BUILD}/Makefile | grep 'PLUGIN = ' | cut -d ' ' -f 3)"
-  $(get_build_dir vdr-helper)/zip_config.sh ${INSTALL} ${PKG_DIR} ${PLUGIN}
+  $(get_build_dir vdr-helper)/zip_config.sh ${INSTALL} ${PKG_DIR} "softhddrm"
 }
--- packages/vdr/_vdr-plugin-softhdvaapi/package.mk
+++ packages/vdr/_vdr-plugin-softhdvaapi/package.mk
@@ -8,7 +8,7 @@ PKG_SITE="https://github.com/jojo61/vdr-plugin-softhdcuvid"
 PKG_URL="https://github.com/jojo61/vdr-plugin-softhdcuvid/archive/${PKG_VERSION}.zip"
 PKG_SOURCE_DIR="vdr-plugin-softhdcuvid-${PKG_VERSION}"
 PKG_DEPENDS_TARGET="toolchain _vdr _libplacebo vdr-helper"
-PKG_DEPENDS_CONFIG="_vdr"
+PKG_DEPENDS_CONFIG="_vdr _libplacebo"
 PKG_NEED_UNPACK="$(get_pkg_directory _vdr vdr-helper)"
 PKG_LONGDESC="VDR Output Device (softhdvaapi)"
 PKG_MAKE_OPTS_TARGET="NVIDIA=$(get_install_dir nvidia)"
@@ -30,6 +30,5 @@ pre_make_target() {
 post_makeinstall_target() {
   mkdir -p ${INSTALL}/storage/.config/vdropt-sample/plugins/softhdvaapi
 
-  PLUGIN="$(cat ${PKG_BUILD}/Makefile | grep 'PLUGIN = ' | cut -d ' ' -f 3)"
-  $(get_build_dir vdr-helper)/zip_config.sh ${INSTALL} ${PKG_DIR} ${PLUGIN}
+  $(get_build_dir vdr-helper)/zip_config.sh ${INSTALL} ${PKG_DIR} "softhdvaapi"
 }

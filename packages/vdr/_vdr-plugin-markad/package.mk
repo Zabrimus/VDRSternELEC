481a92db7dbfb352b246f4e019dfc9316387dac7
Zabrimus <256128+Zabrimus@users.noreply.github.com>
2024-10-31 23:15:55 +0000

    Update packages

--- packages/vdr/_vdr-plugin-markad/package.mk
+++ packages/vdr/_vdr-plugin-markad/package.mk
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 PKG_NAME="_vdr-plugin-markad"
-PKG_VERSION="557bc1e88065c71da90f905b6c6fa26d3538e324"
-PKG_SHA256="e6e9b640ad0878f55ab3cf9d0fda09ff096c8dc1c6e83f1efcc0f5ba7330de13"
+PKG_VERSION="627369618a8a78726d6e68c1253617efffccf50d"
+PKG_SHA256="25b0b0c2cb48f0a2acaa0ab9d2a79c363b4aa1fd98655593b5c715ac93365b7b"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/kfb77/vdr-plugin-markad"
 PKG_URL="https://github.com/kfb77/vdr-plugin-markad/archive/${PKG_VERSION}.zip"

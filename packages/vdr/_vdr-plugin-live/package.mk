b00cd017104316c27c7605332439aaa8e4400667
Zabrimus <256128+Zabrimus@users.noreply.github.com>
2025-01-01 23:13:37 +0000

    Update packages

--- packages/vdr/_vdr-plugin-live/package.mk
+++ packages/vdr/_vdr-plugin-live/package.mk
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 PKG_NAME="_vdr-plugin-live"
-PKG_VERSION="40fc484e4d8244b045470a81084f1c78cf904c94"
-PKG_SHA256="a6a41f501d482961342877aeef2b03668e6f419aea50dac18227062b7e466180"
+PKG_VERSION="d264af2eefcd3b974f222fdf90aa026d9769c7d6"
+PKG_SHA256="9d6c09a904fc577815f916e296faee3f79ce10567fcc96fb920a7199afb0e557"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/MarkusEh/vdr-plugin-live"
 PKG_URL="https://github.com/MarkusEh/vdr-plugin-live/archive/${PKG_VERSION}.zip"

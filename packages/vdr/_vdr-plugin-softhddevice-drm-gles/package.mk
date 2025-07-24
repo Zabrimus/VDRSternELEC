fca5c15bc1d11ca8b5effb3a8308374e5e192355
Zabrimus <256128+Zabrimus@users.noreply.github.com>
2025-07-24 23:18:36 +0000

    Update packages _vdr-plugin-softhddevice-drm-gles

--- packages/vdr/_vdr-plugin-softhddevice-drm-gles/package.mk
+++ packages/vdr/_vdr-plugin-softhddevice-drm-gles/package.mk
@@ -2,8 +2,8 @@
 
 PKG_NAME="_vdr-plugin-softhddevice-drm-gles"
 
-PKG_VERSION="a3a187282b43ba93250d568575f0c0e5f98f62ee"
-PKG_SHA256="749dc0fb9cf9cc220e03208fc83faa3c5cc8dfef9677e78c49a35736d4797829"
+PKG_VERSION="5e5f5de27c1df4f00ef64b2db13cf21dccdad541"
+PKG_SHA256="ad6133d39d69be9b3959691168415d8c263f232cf19e076ca3a08bf1fa9e4186"
 PKG_SITE="https://github.com/rellla/vdr-plugin-softhddevice-drm-gles"
 PKG_URL="https://github.com/rellla/vdr-plugin-softhddevice-drm-gles/archive/${PKG_VERSION}.zip"
 PKG_BRANCH="drm-atomic-gles"

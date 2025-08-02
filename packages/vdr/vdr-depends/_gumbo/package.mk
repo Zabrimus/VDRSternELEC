f66d0adc170a8adf4296d259b0819694dd15d02b
Zabrimus <github@hannebauer.org>
2025-08-02 19:24:58 +0200

    new package: _gumbo

---
+++ packages/vdr/vdr-depends/_gumbo/package.mk
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+PKG_NAME="_gumbo"
+PKG_VERSION="14e3d4a0583aaabe92ad9735d4a127fb2d7ea969"
+PKG_SHA256="840756f31c4c7201895805f783668985017b0a6e09aea19a099786af3738be0c"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/GerHobbelt/gumbo-parser"
+PKG_URL="https://github.com/GerHobbelt/gumbo-parser/archive/${PKG_VERSION}.zip"
+PKG_SOURCE_DIR="gumbo-parser-${PKG_VERSION}"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Gumbo - A pure-C HTML5 parser."
+PKG_TOOLCHAIN="configure"
+PKG_BUILD_FLAGS="+speed"
+
+PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr/local \
+						   --bindir=/usr/local/bin \
+                           --libdir=/usr/local/lib \
+                           --libexecdir=/usr/local/bin \
+                           --sbindir=/usr/local/sbin \
+                           --sysconfdir=/usr/local/etc \
+                           --disable-oss \
+						   --disable-solaris-audio \
+						   --disable-al-audio \
+                           --disable-win \
+                           --with-gnu-ld \
+                           --with-pic \
+                           "
+
+pre_configure_target() {
+  export LDFLAGS="$(echo ${LDFLAGS} | sed -e "s|-Wl,--as-needed||") -L${SYSROOT_PREFIX}/usr/local/lib"
+  ./autogen.sh
+}
+
+make_target() {
+  make CFLAGS="-DNO_GUMBO_DEBUG"
+}

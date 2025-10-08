d36dcdbf95940841d196576f1d4437de90a4bb95
Zabrimus <github@hannebauer.org>
2025-10-08 16:04:34 +0200

    all packages: change ftp.gnu.org to mirror, because of overloaded ftp.gnu.org and download problems (fix)

--- patches/1000-change-ftp.gnu.org.sh
+++ patches/1000-change-ftp.gnu.org.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(find CoreELEC/packages LibreELEC.tv/packages -name "package.mk" -exec grep -l "ftp.gnu.org" {} \;); do
+for i in $(find packages -name "package.mk" -exec grep -l "ftp.gnu.org" {} \;); do
    sed -i -e "s#ftp.gnu.org/pub/gnu#mirror.netcologne.de/gnu#" $i
    sed -i -e "s#ftp.gnu.org/gnu#mirror.netcologne.de/gnu#" $i
 done

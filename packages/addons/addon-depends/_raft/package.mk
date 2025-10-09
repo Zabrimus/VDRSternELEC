35f704c6d73f40c369bcc0656a2d5fbe05bf442e
Zabrimus <github@hannebauer.org>
2025-10-09 07:22:35 +0200

    _raft: update and fix package

--- packages/addons/addon-depends/_raft/package.mk
+++ packages/addons/addon-depends/_raft/package.mk
@@ -1,15 +1,14 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 PKG_NAME="_raft"
-PKG_VERSION="e1045698c0e6fa436158bed8442ac1cecbc7e011"
-PKG_SHA256="5733521ce10ddab3bd1d51e3477372b42eb141159c865d22d1065d131a42fd4b"
+PKG_VERSION="dbdbfa3eef2250050310160134aabe69054deb42"
+PKG_SHA256="0db29cd33c29687deec81235f3f0988ce21984f33e017f89301329da882a4ed0"
 PKG_LICENSE="LGPL 3"
 PKG_SITE="https://github.com/cowsql/raft"
 PKG_URL="https://github.com/cowsql/raft/archive/${PKG_VERSION}.zip"
-PKG_BRANCH="master"
+PKG_BRANCH="main"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SOURCE_DIR="raft-${PKG_VERSION}"
-PKG_BRANCH="master"
 PKG_LONGDESC="Powerful system container and virtual machine manager"
 PKG_TOOLCHAIN="configure"
 PKG_BUILD_FLAGS="+speed -sysroot"

335c16e5929d90eb9794b16bb61faf9847f9001a
Zabrimus <github@hannebauer.org>
2025-12-23 22:53:56 +0100

    channellogos: create an addon

---
+++ packages/addons/channellogos/package.mk
@@ -0,0 +1,40 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+. $(get_pkg_directory _MP_Logos)/package.mk
+
+PKG_NAME="channellogos"
+PKG_URL=""
+PKG_DEPENDS_UNPACK+=" _MP_Logos"
+PKG_BUILD_FLAGS="-sysroot"
+PKG_SOURCE_DIR=""
+
+PKG_REV="1"
+PKG_IS_ADDON="yes"
+# PKG_SECTION="service"
+PKG_ADDON_NAME="channellogos"
+PKG_ADDON_TYPE="xbmc.service"
+
+unpack() {
+  mkdir -p ${PKG_BUILD}
+
+  SRC_PART="${SOURCES}/_MP_Logos/_MP_Logos-${PKG_VERSION}"
+
+  if [ -f "${SRC_PART}.tar.xz" ]; then
+	tar --strip-components=1 -xf ${SOURCES}/_MP_Logos/_MP_Logos-${PKG_VERSION}.xz -C ${PKG_BUILD}
+  elif [ -f "${SRC_PART}.tar.gz" ]; then
+	tar --strip-components=1 -xf ${SOURCES}/_MP_Logos/_MP_Logos-${PKG_VERSION}.gz -C ${PKG_BUILD}
+  elif [ -f "${SRC_PART}.zip" ]; then
+  	unzip ${SRC_PART}.zip -d ${PKG_BUILD}
+
+	mv ${PKG_BUILD}/MP_Logos-${PKG_VERSION}/* ${PKG_BUILD}
+	rm -Rf ${PKG_BUILD}/MP_Logos-${PKG_VERSION}
+  else
+	tar --strip-components=1 -xf ${SRC_PART}.tar.bz2 -C ${PKG_BUILD}
+  fi
+}
+
+addon() {
+	# create directories
+  	mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/logofiles
+  	cp -a $(get_install_dir _mediaportal-de-logos)/usr/local/vdrshare/logofiles/* ${ADDON_BUILD}/${PKG_ADDON_ID}/logofiles
+}

7aa82bf35b9d92999f5b49e8fa864dc0a4bb5c73
Zabrimus <github@hannebauer.org>
2026-06-15 07:14:21 +0200

    _vdr-plugin-live: switch repository to Codeberg

--- packages/vdr/_vdr-plugin-live/package.mk
+++ packages/vdr/_vdr-plugin-live/package.mk
@@ -1,16 +1,16 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 PKG_NAME="_vdr-plugin-live"
-PKG_VERSION="15d74758282eb1b818c48c531111362f53cb112d"
-PKG_SHA256="ffa549e090a18baf1a187df40e7448d66e6d211e878a90f365e2b895e1e350c5"
+PKG_VERSION="635cb0dfba47ff5b2d8ef2a3b5d940d51fe21893"
+PKG_SHA256="8e6520138c1210403a3e15afcef2f12d6a7ca3d4c0b131b5f02c0b230497dd98"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/MarkusEh/vdr-plugin-live"
-PKG_URL="https://github.com/MarkusEh/vdr-plugin-live/archive/${PKG_VERSION}.zip"
+PKG_SITE="https://codeberg.org/MarkusE/vdr-plugin-live"
+PKG_URL="https://codeberg.org/MarkusE/vdr-plugin-live/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain _vdr tntnet pcre2 libiconv cxxtools vdr-helper"
 PKG_DEPENDS_CONFIG="_vdr"
 PKG_NEED_UNPACK="$(get_pkg_directory _vdr) $(get_pkg_directory vdr-helper)"
 PKG_DEPENDS_UNPACK="vdr-helper"
-PKG_SOURCE_DIR="vdr-plugin-live-${PKG_VERSION}"
+PKG_SOURCE_DIR="vdr-plugin-live"
 PKG_LONGDESC="Allows a comfortable operation of VDR and some of its plugins trough a web interface."
 PKG_BUILD_FLAGS="+pic -parallel +speed"
 

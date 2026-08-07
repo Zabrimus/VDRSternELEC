2b285399ef62b6faf96cdf31bedcda6af4930a23
Zabrimus <github@hannebauer.org>
2026-08-07 12:46:29 +0200

    modified _vdr-plugin-live to use new(old) packages _cxxtools30 and _tntnet30 (clean build required)

---
+++ packages/vdr/vdr-depends/_cxxtools30/package.mk
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="_cxxtools30"
+PKG_VERSION="3.0"
+PKG_SHA256="07b18037fb0983f6292f5c8d53e2369e9e7a9711df2c9ad50838aacbc8c62f7c"
+PKG_LICENSE="LGPL-2.1-or-later"
+PKG_SITE="http://www.tntnet.org/cxxtools.html"
+PKG_URL="http://ftp.rz.tu-braunschweig.de/pub/mirror/ubuntu-packages/pool/universe/c/cxxtools/cxxtools_${PKG_VERSION}.0.orig.tar.gz"
+PKG_DEPENDS_HOST="toolchain:host openssl:host"
+PKG_DEPENDS_TARGET="toolchain _cxxtools30:host openssl"
+PKG_LONGDESC="Cxxtools is a collection of general-purpose C++ classes."
+PKG_BUILD_FLAGS="+pic"
+
+PKG_CONFIGURE_OPTS_HOST="--disable-demos --disable-unittest"
+PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --disable-demos --disable-unittest"
+
+post_makeinstall_host() {
+  rm -rf ${TOOLCHAIN}/bin/cxxtools-config
+}
+
+post_makeinstall_target() {
+  cp cxxtools-config ${TOOLCHAIN}/bin
+  sed -e "s:\(['= ]\)/usr:\\1${PKG_ORIG_SYSROOT_PREFIX}/usr:g" -i ${TOOLCHAIN}/bin/cxxtools-config
+  chmod +x ${TOOLCHAIN}/bin/cxxtools-config
+
+  rm -rf ${INSTALL}/usr/bin
+}
---
+++ packages/vdr/vdr-depends/_tntnet30/package.mk
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="_tntnet30"
+PKG_VERSION="3.0"
+PKG_SHA256="718e5519b0a403f7f766358bf66a85c008119c48189d1c2b7651fd0af9018e27"
+PKG_LICENSE="LGPL-2.1-or-later"
+PKG_SITE="http://www.tntnet.org/"
+PKG_URL="http://slackware.uk/sbosrcarch/by-name/network/tntnet/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_HOST="_cxxtools30:host zlib:host"
+PKG_DEPENDS_TARGET="toolchain _tntnet30:host libtool _cxxtools30 zlib"
+PKG_LONGDESC="A web application server for C++."
+
+PKG_CONFIGURE_OPTS_HOST="--disable-unittest \
+                         --disable-server \
+                         --enable-sdk \
+                         --disable-demos"
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-unittest \
+                           --with-sysroot=${SYSROOT_PREFIX} \
+                           --disable-server \
+                           --disable-sdk \
+                           --disable-demos"
+
+post_configure_target() {
+  libtool_remove_rpath libtool
+}
+
+post_makeinstall_target() {
+  rm -rf ${INSTALL}/usr/bin
+  rm -rf ${INSTALL}/usr/share
+}
